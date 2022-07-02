2372c2f7d7df649baeff3238336b02e61f9f9e77
Damir <damirr2001@gmail.com>
2022-07-03 00:50:57 +0600

    d

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,35 @@
 package org.example;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
-    public static void programForDate(String date){
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDate localDate = LocalDate.parse(date, formatter);
-            System.out.printf("%s%n", localDate);
+    public static void main(String[] args) throws IOException {
+        URL postUrl = new URL("https://dummy.restapiexample.com/api/v1/create");
+        HttpURLConnection http = (HttpURLConnection)postUrl.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("Accept", "application/json");
+        http.setRequestProperty("Content-Type", "application/json");
+        JSONObject jsonObject = new JSONObject();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        jsonObject.put("name",in.next());
+        jsonObject.put("salary","123");
+        jsonObject.put("age","23");
+        byte[] out = jsonObject.toJSONString().getBytes(StandardCharsets.UTF_8);
 
-            // будет через 30 дней
-            System.out.print("Через 30 дней будет ");
-            System.out.println(localDate.plusDays(30));
+        OutputStream stream = http.getOutputStream();
+        stream.write(out);
 
-            // будет последним воскресеньем в месяце от этой даты
-            YearMonth ym = YearMonth.of(localDate.getYear(), localDate.getMonth());
-            LocalDate endDate = ym.atEndOfMonth();
-            DayOfWeek day = endDate.getDayOfWeek();
-            int lastDay = day.getValue();
-            System.out.print("Эта дата будет последним воскресеньем в этом месяце: ");
-            if(lastDay < 7)
-                endDate = endDate.minusDays(lastDay);
-            System.out.println(endDate);
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
 
-            //Программа должна рассчитать сколько дней составляет между этой датой и Новым Годом 31 декабря того же года
-            Calendar calOne = Calendar.getInstance();
-            int dayOfYear = calOne.get(Calendar.DAY_OF_YEAR);
-            int year = calOne.get(Calendar.YEAR);
-            Calendar calTwo = new GregorianCalendar(year, 11, 31);
-            int Day = calTwo.get(Calendar.DAY_OF_YEAR);
-            int total_days = Day - dayOfYear;
-            System.out.println(total_days+" дней составляет между этой датой и Новым Годом");
-
-        } catch (DateTimeParseException exc) {
-            System.out.printf("%s is not parsable!%n", date);
-        }
-
-    }
-    public static void main(String[] args) {
-        programForDate("2022-06-18");
+        http.disconnect();
     }
 }
